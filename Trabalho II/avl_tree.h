9b2f81b3aa1d83ca24db18fe9ef92ca9824623d0
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-04-20 01:18:41 -0400

    AVL Tree Library Headers
    AVL Tree Library Haeders definition

--- Trabalho II/avl_tree.h
+++ Trabalho II/avl_tree.h
@@ -10,3 +10,21 @@ typedef struct __avl__tree__node{
 
 } avl_tree;
 
+#define MAX(a, b) a > b ? a : b
+
+avl_tree * make__avl__tree();
+int __avl__height(const avl_tree *__root);
+avl_tree * avl__tree__push(avl_tree *, const int __data);
+int __subtree__height(const avl_tree *__root);
+avl_tree * __avl__rotate__right(avl_tree *__node);
+avl_tree * __avl__rotate__left(avl_tree *__node);
+avl_tree * __RR(avl_tree *__node);
+avl_tree * __LL(avl_tree *__node);
+avl_tree * __RL(avl_tree *__node);
+avl_tree * __LR(avl_tree *__node);
+int __balancing__factor(const avl_tree *__root);
+void avl__tree__infix(const avl_tree *__root);
+void avl__tree__posfix(const avl_tree *__root);
+void avl__tree__prefix(const avl_tree *__root);
+int avl__tree__height(const avl_tree *__root);
+avl_tree * avl__tree__pop(avl_tree *__root, const int __key);
