bf20275720ccd472372af0787910a4b9490a0e27
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-04-21 12:48:34 -0400

    minor change

--- Trabalho II/b_tree.h
+++ Trabalho II/b_tree.h
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #pragma once
 
 typedef struct __binary__tree__node{
@@ -15,7 +14,7 @@ void binary_tree_pop(b_tree *, const int __key);
 void binary__tree__infix(const b_tree *);
 void binary__tree__posfix(const b_tree *);
 void binary__tree__prefix(const b_tree *);
-bool binary__tree__search(const b_tree *, const int __key);
+b_tree * binary__tree__search(b_tree *, const int __key);
 b_tree * binary__tree__pop(b_tree *__root, const int __key);
 b_tree * __find__max__value__node(b_tree *__root);
 b_tree * __find__min__value__node(b_tree *__root);
