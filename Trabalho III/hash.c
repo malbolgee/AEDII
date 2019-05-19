b4d57210009c03ec92fe42b31df7a0c3b338bc6a
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-05-19 13:19:27 -0400

    function definition

--- Trabalho III/hash.c
+++ Trabalho III/hash.c
@@ -1,11 +1,80 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "hash.h"
 
 static uint32_t __rotl(uint32_t __x, int8_t __r);
 static uint32_t __avalanche(uint32_t __h);
 static uint32_t murmurhash(const void *__key, const uint32_t __len, uint32_t __seed);
+
+hash_t * make__hash__table(const unsigned __size)
+{
+
+	unsigned i;
+	hash_t *__hash = (hash_t *) calloc(__size, sizeof(hash_t));
+	if (!__hash)
+		exit(1);
+
+	return __hash;
+
+}
+
+void hash__push(hash_t *__hash, const unsigned __register_pointer, const char *__data)
+{
+
+	element_t *aux;
+	aux = (element_t *) malloc(sizeof(element_t));
+
+	if (!aux)
+		exit(1);
+
+	uint32_t idx = murmurhash(__data, strlen(__data), 123);
+
+	if (__hash[idx].first)
+		__hash[idx].last->next = aux;
+	else
+		__hash[idx].first = aux;
+
+	__hash[idx].last = aux;
+	aux->next = NULL;
+	aux->data = atoi(__data);
+	aux->register_pointer = __register_pointer;
+
+}
+
+int hash__search(hash_t *__hash, const char *__data)
+{
+
+	element_t *aux;
+	unsigned idx = murmurhash(__data, strlen(__data), 123);
+
+	unsigned id = atoi(__data);
+	aux = __hash[idx].first;
+
+	if (!aux)
+		return -1;
+
+	if (aux->data == id)
+		return aux->register_pointer;
+	else
+	{
+
+		while (aux)
+		{
+
+			if (aux->data == id)
+				return aux->register_pointer;
+
+			aux = aux->next;			
+
+		}
+
+		return -1;
+		
+	}
+
+}
 
 static uint32_t __rotl(uint32_t __x, int8_t __r)
 {
@@ -27,7 +96,7 @@ static uint32_t __avalanche(uint32_t __h)
 
 }
 
-uint32_t murmurhash(const void *__key, const uint32_t __len, uint32_t __seed)
+static uint32_t murmurhash(const void *__key, const uint32_t __len, uint32_t __seed)
 {
 
 	char i;
@@ -80,6 +149,6 @@ uint32_t murmurhash(const void *__key, const uint32_t __len, uint32_t __seed)
 	h ^= __len;
 	h = __avalanche(h);
 
-	return h % 1000003;
+	return h % 10177;
 
 }
