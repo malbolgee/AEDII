be187a3b6eed9c23daa618d78cefa0c4a52cc56d
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-05-21 23:24:37 -0400

    several changes and some function definitions

--- Trabalho III/bin_tree.c
+++ Trabalho III/bin_tree.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/* Create a instance of a bin_tree_t pointer by putting NULL in it. */
 void make__binary__tree(bin_tree_t **__root)
 {
 
@@ -10,6 +11,7 @@ void make__binary__tree(bin_tree_t **__root)
 
 }
 
+/* Inserts data and its registry pointer into a BST. */
 bin_tree_t * binary__tree__push(bin_tree_t *__root, const unsigned __data, const unsigned __rp)
 {
 
@@ -31,6 +33,7 @@ bin_tree_t * binary__tree__push(bin_tree_t *__root, const unsigned __data, const
 
 }
 
+/* Search for key on a BST. */
 int binary__tree__search(bin_tree_t *__root, const unsigned __key)
 {
 	
@@ -46,6 +49,34 @@ int binary__tree__search(bin_tree_t *__root, const unsigned __key)
 	
 }
 
+/* Range query in the BST index for values greater or  equal 
+   to k1 and smaller or equal to k2 on STREAM. */
+void binary__tree__range__query(bin_tree_t *__root, const unsigned __k1, const unsigned __k2, FILE *__stream)
+{
+
+	if (!__root)
+		return;
+
+	if (__k1 <= __root->id)
+		binary__tree__range__query(__root->left, __k1, __k2, __stream);
+
+	if (__k1 <= __root->id && __k2 >= __root->id)
+	{
+
+		product_t tmp;
+		fseek(__stream, __root->registry_pointer * sizeof(product_t), SEEK_SET);
+		fread(&tmp, sizeof(product_t), 1, __stream);
+		info_print(tmp);
+
+	}
+
+	if (__k2 >= __root->id)
+		binary__tree__range__query(__root->right, __k1, __k2, __stream);
+
+
+}
+
+/* Does a infix traversal on a BST. */
 void binary__tree__infix(const bin_tree_t *__root)
 {
 
@@ -57,5 +88,14 @@ void binary__tree__infix(const bin_tree_t *__root)
 		binary__tree__infix(__root->right);
 
 	}
+
+}
+
+/* Prints a product_t type fields. */
+void info_print(product_t produto)
+{
+
+	printf("Id: %d\nNome: %s\nDescrição: %s\nPreço: %.2f\nDisponibilidade: %d\nVencimento: %d\n\n",
+		produto.id, produto.nome, produto.descricao, produto.preco, produto.disponibilidade, produto.vencimento);
 
 }
