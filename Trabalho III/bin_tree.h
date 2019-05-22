8ca217faffb37a8b3fc553d06ef3d324e4d86468
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-05-21 23:25:05 -0400

    added function headers

--- Trabalho III/bin_tree.h
+++ Trabalho III/bin_tree.h
@@ -1,4 +1,16 @@
+#include <stdio.h>
 #pragma once
+
+typedef struct __product__type{
+
+	unsigned id;
+	char nome[51];
+	char descricao[101];
+	float preco;
+	unsigned disponibilidade;
+	unsigned vencimento;
+
+} product_t;
 
 typedef struct __binary__tree__node{
 
@@ -13,3 +25,5 @@ void make__binary__tree(bin_tree_t **__root);
 bin_tree_t * binary__tree__push(bin_tree_t *__root, const unsigned __data, const unsigned __rp);
 int binary__tree__search(bin_tree_t *, const unsigned __key);
 void binary__tree__infix(const bin_tree_t *__root);
+void binary__tree__range__query(bin_tree_t *__root, const unsigned __k1, const unsigned __k2s, FILE *__stream);
+void info_print(product_t produto);
