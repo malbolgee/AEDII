2602470bcb78f79165c89ac0e2da5961179f548a
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-05-26 23:07:29 -0400

    several changes

--- Trabalho III/bin_tree.c
+++ Trabalho III/bin_tree.c
@@ -1,10 +1,11 @@
 #include "bin_tree.h"
+#include "product.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-/* Create a instance of a bin_tree_t pointer by putting NULL in it. */
-void make__binary__tree(bin_tree_t **__root)
+/* Create a instance of a BST_t pointer by putting NULL in it. */
+void make__binary__tree(BST_t **__root)
 {
 
 	*__root = NULL;
@@ -12,90 +13,46 @@ void make__binary__tree(bin_tree_t **__root)
 }
 
 /* Inserts data and its registry pointer into a BST. */
-bin_tree_t * binary__tree__push(bin_tree_t *__root, const unsigned __data, const unsigned __rp)
+BST_t * BST__id__push(BST_t *__root, const unsigned __data, const unsigned __rp)
 {
 
 	if (!__root)
 	{
 
-		__root = (bin_tree_t *) malloc(sizeof(bin_tree_t));
+		__root = (BST_t *) malloc(sizeof(BST_t));
 		__root->id = __data;
 		__root->registry_pointer = __rp;
 		__root->left = __root->right = NULL;
 
 	}
 	else if (__root->id < __data)
-		__root->right = binary__tree__push(__root->right, __data, __rp);
+		__root->right = BST__id__push(__root->right, __data, __rp);
 	else if (__root->id > __data)
-		__root->left = binary__tree__push(__root->left, __data, __rp);
+		__root->left = BST__id__push(__root->left, __data, __rp);
 
 	return __root;
 
 }
 
 /* Search for key on a BST. */
-int binary__tree__search(bin_tree_t *__root, const unsigned __key)
+void BST__id__search(BST_t *__root, const unsigned __key, FILE *__STREAM)
 {
 	
-	if (!__root)
-		return -1;
-	
-	if (__root->id < __key)
-		return binary__tree__search(__root->right, __key);
-	else if (__root->id > __key)
-		return binary__tree__search(__root->left, __key);
-	else
-		return __root->registry_pointer;
-	
-}
-
-/* Range query in the BST index for values greater or  equal 
-   to k1 and smaller or equal to k2 on STREAM. */
-void binary__tree__range__query(bin_tree_t *__root, const unsigned __k1, const unsigned __k2, FILE *__stream)
-{
-
 	if (!__root)
 		return;
-
-	if (__k1 <= __root->id)
-		binary__tree__range__query(__root->left, __k1, __k2, __stream);
-
-	if (__k1 <= __root->id && __k2 >= __root->id)
+	
+	if (__root->id < __key)
+		return BST__id__search(__root->right, __key, __STREAM);
+	else if (__root->id > __key)
+		return BST__id__search(__root->left, __key, __STREAM);
+	else
 	{
 
 		product_t tmp;
-		fseek(__stream, __root->registry_pointer * sizeof(product_t), SEEK_SET);
-		fread(&tmp, sizeof(product_t), 1, __stream);
-		info_print(tmp);
+		fseek(__STREAM, __root->registry_pointer * sizeof(product_t), SEEK_SET);
+		fread(&tmp, sizeof(product_t), 1, __STREAM);
+		return;
 
 	}
-
-	if (__k2 >= __root->id)
-		binary__tree__range__query(__root->right, __k1, __k2, __stream);
-
-
-}
-
-/* Does a infix traversal on a BST. */
-void binary__tree__infix(const bin_tree_t *__root)
-{
-
-	if (__root)
-	{
-
-		binary__tree__infix(__root->left);
-		printf("%d ", __root->id);
-		binary__tree__infix(__root->right);
-
-	}
-
-}
-
-/* Prints a product_t type fields. */
-void info_print(product_t produto)
-{
-
-	printf("Id: %d\nNome: %s\nDescrição: %s\nPreço: %.2f\nDisponibilidade: %d\nVencimento: %d\n\n",
-		produto.id, produto.nome, produto.descricao, produto.preco, produto.disponibilidade, produto.vencimento);
-
+	
 }
