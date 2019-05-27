1959dbb785f5372e1af08403c34081b28dc7a4b4
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-05-26 23:07:38 -0400

    several changes

--- Trabalho III/bin_tree.h
+++ Trabalho III/bin_tree.h
@@ -1,29 +1,16 @@
 #include <stdio.h>
+#include "product.h"
 #pragma once
 
-typedef struct __product__type{
-
-	unsigned id;
-	char nome[51];
-	char descricao[101];
-	float preco;
-	unsigned disponibilidade;
-	unsigned vencimento;
-
-} product_t;
-
-typedef struct __binary__tree__node{
+typedef struct __BST__id__index{
 
 	unsigned id;
 	unsigned registry_pointer;
-	struct __binary__tree__node *right;
-	struct __binary__tree__node *left;
+	struct __BST__id__index *right;
+	struct __BST__id__index *left;
 
-} bin_tree_t;
+} BST_t;
 
-void make__binary__tree(bin_tree_t **__root);
-bin_tree_t * binary__tree__push(bin_tree_t *__root, const unsigned __data, const unsigned __rp);
-int binary__tree__search(bin_tree_t *, const unsigned __key);
-void binary__tree__infix(const bin_tree_t *__root);
-void binary__tree__range__query(bin_tree_t *__root, const unsigned __k1, const unsigned __k2s, FILE *__stream);
-void info_print(product_t produto);
+void    make__binary__tree(BST_t **__root);
+BST_t * BST__id__push(BST_t *__root, const unsigned __data, const unsigned __rp);
+void    BST__id__search(BST_t *__root, const unsigned __key, FILE *__STREAM);
