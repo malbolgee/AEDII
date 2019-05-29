3fe901eff55bef6c01146db7e0cc471c90275038
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-05-29 00:20:46 -0400

    search function headers declarations

--- Trabalho III/product.h
+++ Trabalho III/product.h
@@ -15,3 +15,5 @@ typedef struct __product__type{
 int  get_product_info(product_t *__product, FILE *__STREAM);
 void info_print(product_t produto);
 int  linear__registry__search(const unsigned __key, FILE *__STREAM);
+int  linear__conditional__price__query(FILE *__STREAM, const float __key, const char __delim);
+int  linear__range__price__query(FILE *__STREMA, const float __delim1, const float __delim2);
