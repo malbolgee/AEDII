b74e241ff444bf4486a3d5cde39ec889872f1a40
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-05-29 00:20:00 -0400

    function headers declarations

--- Trabalho III/bin_tree_f.h
+++ Trabalho III/bin_tree_f.h
@@ -10,4 +10,9 @@ typedef struct __BST__prince__index{
 
 } BST_p_t;
 
-void    BST__price__range__query(BST_p_t *__root, const float __k1, const float __k2, FILE *__STREAM);
+void      make__price__index__tree(BST_p_t **__root);
+BST_p_t * BST__price__push(BST_p_t *__root, const float __data, const unsigned __rp);
+void      BST__price__range__query(BST_p_t *__root, const float __k1, const float __k2, FILE *__STREAM);
+void      BST__price__range__gt(BST_p_t *__root, const float __k1, FILE *__STREAM);
+void      BST__price__range__lt(BST_p_t *__root, const float __k1, FILE *__STREAM);
+void	  infix(BST_p_t *__root);
