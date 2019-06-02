d835617afde80c0679a4724cec86da396c49b026
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-06-01 21:45:44 -0400

    several changes

--- Trabalho III/hash.h
+++ Trabalho III/hash.h
@@ -2,26 +2,24 @@
 #include <stdio.h>
 #pragma once
 
-typedef struct __node__hash{
+typedef struct __hash__array__type{
 
 	int data;
 	unsigned registry_pointer;
-	struct __node__hash *next;
-
-} element_t;
-
-typedef struct __hash__table{
-
-	element_t *first;
-	element_t *last;
 
 } hash_t;
 
 #define ROTL32(x, y) __rotl(x, y);
 
-#define MAXHASHSIZE 20011
+#ifndef PRIME_HASH_SIZE
+	#define PRIME_HASH_SIZE 100003U
+#endif
+
+#define COL_START_P PRIME_HASH_SIZE + 1
+#define COL_END_P (unsigned)((PRIME_HASH_SIZE * 0.15F) + COL_START_P + 1)
+#define MAX_HASH_SIZE COL_END_P
+#define EMPTY -1
 
 hash_t * make__hash__table(const unsigned __size);
-void hash__push(hash_t *__hash, const char *__key, const unsigned __rp);
-void hash__search(hash_t *__hash, const char *__data, FILE *__STREAM);
-unsigned hash__colisions(const hash_t *__hash, const unsigned __size);
+void     hash__push(hash_t *__hash, const unsigned __data, const unsigned __rp);
+void     hash__find(hash_t *__hash, const unsigned __data, FILE *__STREAM);
