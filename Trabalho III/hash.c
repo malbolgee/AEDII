73937d5ed20e1cdde17ac5790d69008e22d304b1
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-06-01 21:45:29 -0400

    several changes and fixes
    Re-implementation of hash table data structure

--- Trabalho III/hash.c
+++ Trabalho III/hash.c
@@ -5,189 +5,93 @@
 #include "hash.h"
 #include "product.h"
 
-static uint32_t __rotl(uint32_t __x, int8_t __r);
-static uint32_t __avalanche(uint32_t __h);
-static uint32_t murmurhash(const void *__key, const uint32_t __len, uint32_t __seed);
+static uint32_t hash(uint32_t x);
 
-/* Makes a hash table of length SIZE. */
+extern unsigned __stt;
+extern unsigned __end;
+extern unsigned hash_colisions;
+
 hash_t * make__hash__table(const unsigned __size)
 {
 
-	unsigned i;
-	hash_t *__hash = (hash_t *) calloc(__size, sizeof(hash_t));
-	if (!__hash)
+	hash_t *hash = (hash_t *) malloc(sizeof(hash_t) * __size);
+	if (hash == NULL)
 		exit(1);
 
-	return __hash;
+	memset(hash, -1, sizeof(hash) * __size);
+
+	return hash;
 
 }
 
-/* Puts the key and its registry pointer into the hash table. */
-void hash__push(hash_t *__hash, const char *__key, const unsigned __rp)
+void hash__push(hash_t *__hash, const unsigned __data, const unsigned __rp)
 {
 
-	element_t *aux;
-	aux = (element_t *) malloc(sizeof(element_t));
-
-	if (!aux)
-		exit(1);
-
-
-	uint32_t idx = murmurhash(__key, strlen(__key), 0) % MAXHASHSIZE;
-
-	if (__hash[idx].first)
-		__hash[idx].last->next = aux;
-	else
-		__hash[idx].first = aux;
-
-	__hash[idx].last = aux;
-	aux->next = NULL;
-	aux->data = atoi(__key);
-	aux->registry_pointer = __rp;
-
-}
-
-/* Does a search for the key in the hash table index. */
-void hash__search(hash_t *__hash, const char *__key, FILE *__STREAM)
-{
-
-	element_t *aux;
-	unsigned idx = murmurhash(__key, strlen(__key), 0) % MAXHASHSIZE;
-
-	unsigned id = atoi(__key);
-	aux = __hash[idx].first;
-
-	if (!aux)
-		return;
-
-	if (aux->data == id)
+	int idx = hash(__data) % (PRIME_HASH_SIZE);
+	if (__hash[idx].data == EMPTY)
 	{
-		
-		product_t tmp;
-		fseek(__STREAM, aux->registry_pointer * sizeof(product_t), SEEK_SET);
-		fread(&tmp, sizeof(product_t), 1, __STREAM);		
-		return;	
 
+		__hash[idx].data = __data;
+		__hash[idx].registry_pointer = __rp;
+
+	}
+	else if (__hash[idx].data != EMPTY)
+	{
+
+		++hash_colisions;
+		__hash[__end].data = __data;
+		__hash[__end++].registry_pointer = __rp;
+
+	}
+
+}
+
+void hash__find(hash_t *__hash, const unsigned __data, FILE *__STREAM)
+{
+
+	unsigned idx = hash(__data) % (PRIME_HASH_SIZE);
+
+	if (__hash[idx].data == __data)
+	{
+
+		product_t tmp;
+		fseek(__STREAM, __hash[idx].registry_pointer * sizeof(product_t), SEEK_SET);
+		fread(&tmp, sizeof(product_t), 1, __STREAM);
+		return;
+		
 	}
 	else
 	{
 
-		while (aux)
+		unsigned i;
+		for (i = COL_START_P; i < __end; ++i)
 		{
 
-			if (aux->data == id)
+			if (__hash[i].data == __data)
 			{
 
 				product_t tmp;
-				fseek(__STREAM, aux->registry_pointer * sizeof(product_t), SEEK_SET);
+				fseek(__STREAM, __hash[i].registry_pointer * sizeof(product_t), SEEK_SET);
 				fread(&tmp, sizeof(product_t), 1, __STREAM);
 				return;
 
 			}
 
-			aux = aux->next;			
-
 		}
 
 		return;
-		
-	}
-
-}
-
-/* Does a left rotation of R bits in X. */
-static uint32_t __rotl(uint32_t __x, int8_t __r)
-{
-
-	return (__x << __r) | (__x >> (32 - __r));
-
-}
-
-/* Bitmixer of h. */
-static uint32_t __avalanche(uint32_t __h)
-{
-
-	__h ^= __h >> 16;
-	__h *= 0x85ebca6b;
-	__h ^= __h >> 13;
-	__h *= 0xc2b2ae35;
-	__h ^= __h >> 16;
-
-	return __h;
-
-}
-
-/* Counts the number of colisions in the hash table. */
-unsigned hash__colisions(const hash_t *__hash, const unsigned __size)
-{
-
-	element_t *j;
-	unsigned i, ans = 0;
-
-	for (i = 0; i < __size; ++i)
-		if (__hash[i].first && __hash[i].first->next)
-			for (j = __hash[i].first->next; j; j = j->next)
-				++ans;
-
-	return ans;
-
-}
-
-
-/* Hash function returns a 32bit hash number. */
-static uint32_t murmurhash(const void *__key, const uint32_t __len, uint32_t __seed)
-{
-
-	char i;
-	uint32_t k;
-	const int nblocks = __len >> 2;
-	const uint8_t *data = (const uint8_t *)__key;
-
-	uint32_t h = __seed;
-
-	const uint32_t c1 = 0xcc9e2d51;
-	const uint32_t c2 = 0x1b873593;
-
-	const uint32_t *blocks = (const uint32_t *)(data + nblocks * 4);
-
-	for (i = -nblocks; i; ++i)
-	{
-
-		k = blocks[i];
-		k = ROTL32(k, 15);
-		k *= c2;
-
-		h ^= k;
-		h = ROTL32(h, 13);
-		h = h * 5 + 0xe6546b64;
 
 	}
 
-	k = 0;
-	const uint8_t *tail = (const uint8_t *)(data + nblocks * 4);
+}
+
+static uint32_t hash(uint32_t x) 
+{
+
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
 	
-	switch(__len & 3)
-	{
-
-		case 3:
-			k ^= tail[2] << 16;
-			break;
-		case 2:
-			k ^= tail[1] << 8;
-			break;
-		case 1:
-			k ^= tail[0];
-			k *= c1; 
-			k = ROTL32(k, 15);
-			k *= c2; 
-			h ^= k;
-			break;
-
-	};
-
-	h ^= __len;
-	h = __avalanche(h);
-
-	return h;
+    return x;
 
 }
