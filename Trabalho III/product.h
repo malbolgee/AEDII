948ffb4b28d4d7efa469bcfbb71b20ca9080c387
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-06-03 00:44:48 -0400

    minor changes

--- Trabalho III/product.h
+++ Trabalho III/product.h
@@ -12,6 +12,9 @@ typedef struct __product__type{
 
 } product_t;
 
+#define MAX_UN_SIZE 100000U
+#define MAX_ID 2000000000U
+
 int  get_product_info(product_t *__product, FILE *__STREAM);
 void info_print(product_t produto);
 int  linear__registry__search(const unsigned __key, FILE *__STREAM);
