7694dd0a5c02464f970cf4532b02405d9d501c4b
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-07-03 18:07:30 -0400

    graph structure and functions

--- Trabalho IV/graph.h
+++ Trabalho IV/graph.h
@@ -1,1 +1,14 @@
 #pragma once
+
+typedef struct __graph__type{
+
+	char *seen;
+	char **adj;
+	unsigned vertex;
+	
+} graph_t;
+
+void make_graph(graph_t *__graph, const unsigned __vertex);
+void print_graph(const graph_t __graph, const unsigned __vertex);
+void dfs(graph_t __graph, const unsigned __u);
+void make_connected(graph_t *__graph, const unsigned __vertex, const float __connectivness);
