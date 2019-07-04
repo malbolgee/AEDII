ce0f50d1a11080fea27fc73fdd3e25bf809d508f
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-07-04 00:40:04 -0400

    minor changes

--- Trabalho IV/graph.h
+++ Trabalho IV/graph.h
@@ -9,6 +9,8 @@ typedef struct __graph__type{
 } graph_t;
 
 void make_graph(graph_t *__graph, const unsigned __vertex);
-void print_graph(const graph_t __graph, const unsigned __vertex);
-void dfs(graph_t __graph, const unsigned __u);
-void make_connected(graph_t *__graph, const unsigned __vertex, const float __connectivness);
+void print_graph(const graph_t __graph);
+void dfs(graph_t __graph, const unsigned __vertex);
+void make_connected(graph_t *__graph, const float __connectivness);
+void free_graph(graph_t *__graph);
+void bfs(graph_t __graph, const unsigned __vertex);
